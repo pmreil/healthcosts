a6d40b94da83a67031fb94b1daee39a0050325bf
Paul <pmreil@ubuntu1204.(none)>
2013-05-10 11:07:52 -0400

    initial commit of health costs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Healthcosts::Application.config.secret_token = '0078269fd8e41c54cc41668c379232caf5a871a4048531b66b5ee0cbced74a0774dd66da926639ad77c2ca26726c1c563615ad52210047cdda2f1ddf2058a21f'
