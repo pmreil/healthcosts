c50c9c1244933112b76f2a71b975a509402ccbe1
Paul Reilly <preilly@teladoc.com>
2013-05-22 17:20:38 -0600

    improved displays

--- app/controllers/drgs_controller.rb
+++ app/controllers/drgs_controller.rb
@@ -14,7 +14,7 @@ class DrgsController < ApplicationController
   # GET /drgs/1.json
   def show
     @drg = Drg.find(params[:id])
-    @costs =  @drg.costs.order('average_total_payments').includes(:hospital)
+    @costs =  @drg.costs.order('average_covered_charges desc').includes(:hospital)
     @average_covered_charges = @costs.average('average_covered_charges')
     @average_total_payments = @costs.average('average_total_payments')
 
