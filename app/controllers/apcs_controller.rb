58016e52047b0fbb28b077e6f3dd24edce645288
Paul Reilly <preilly@teladoc.com>
2013-06-19 21:05:20 -0400

    more to add apcs

--- app/controllers/apcs_controller.rb
+++ app/controllers/apcs_controller.rb
@@ -1,31 +1,8 @@
 class ApcsController < ApplicationController
   def index
     @apcs = Apc.find(:all,:order => "description")
-
-
- acosts = ApcCost.all
-
-    acosts.each do |x| 
-    	puts x.id
-    	if x.position.nil?
-	      apc = x.apc.id
-  	    ext_id = x.hospital.ext_id
-    	  sql = "SELECT x.id, x.position, x.average_estimated_submitted_charges, x.count, ((x.count-x.position)/x.count) as percentile FROM
-(SELECT t.id, t.apc_code,t.average_estimated_submitted_charges,t.hospital_ext_id, @rownum := @rownum+1 AS position, (select count(*) from apc_costs where apc_code = "+apc.to_s+") as count
-FROM apc_costs t
-JOIN (SELECT @rownum := 0) r
-WHERE t.apc_code = "+apc.to_s+"
-ORDER BY t.average_estimated_submitted_charges) x
-WHERE x.hospital_ext_id = '"+ext_id.to_s+"'"
-
-      @records_array = ActiveRecord::Base.connection.execute(sql)
-      a = @records_array
-      	x.position = a.first[1]
- 	    	x.count = a.first[3]
-  	    x.percentile = a.first[4]
-    	  x.save
-    	 end
-    	end
+    @states = State.find(:all,:order => "name")
+    @regions = Hospital.select('referral_region').group(:referral_region).count
   end
 
   def show
@@ -33,8 +10,33 @@ WHERE x.hospital_ext_id = '"+ext_id.to_s+"'"
     @apc_costs =  @apc.apc_costs.order('average_estimated_submitted_charges desc').includes(:hospital)
     @average_estimated_submitted_charges = @apc_costs.average('average_estimated_submitted_charges')
     @average_total_payments = @apc_costs.average('average_total_payments')
+    @states = State.find(:all,:order => "name")
+    @regions = Hospital.select('referral_region').group(:referral_region).count
   end
 
+  def state
+    @state = State.find_by_abbrev(params[:state_id])
+    @apc = Apc.find(params[:id])
+    @thetitle = "Compare Medical billing costs for "+@apc.uc_description+" in "+@state.name.capitalize+" Hospitals"
+    @costs =  @apc.apc_costs.where("hospitals.state = '"+params[:state_id]+"'").order('average_estimated_submitted_charges desc').includes(:hospital)
+    @average_estimated_submitted_charges = @apc_costs.average('average_estimated_submitted_charges')
+    @average_total_payments = @apc_costs.average('average_total_payments')
+    @states = State.find(:all,:order => "name")
+    @regions = Hospital.select('referral_region').group(:referral_region).count
+    render :show  
+  end
+
+  def region
+    @region = params[:region_id]
+    @drg = Apc.find(params[:id])
+    @thetitle = "Compare Medical billing costs for "+@apc.uc_description+" in "+@region+" Hospitals"
+    @costs =  @apc.costs.where("hospitals.referral_region = '"+params[:region_id]+"'").order('average_estimated_submitted_charges desc').includes(:hospital)
+    @average_estimated_submitted_charges = @apc_costs.average('average_estimated_submitted_charges')
+    @average_total_payments = @apc_costs.average('average_total_payments')
+    @states = State.find(:all,:order => "name")
+    @regions = Hospital.select('referral_region').group(:referral_region).count
+    render :show  
+  end
 
 
 end
