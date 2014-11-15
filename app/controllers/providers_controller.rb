75fa4a9c23142e7dddc0d8180e988c66e6ed5b0a
Paul Reilly <preilly@teladoc.com>
2014-11-15 09:37:10 -0500

    added provider search queries

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -4,13 +4,25 @@ class ProvidersController < ApplicationController
     #@specialties = Specialty.all
     @specialties_array = Specialty.order(:name).map { |specialty| [specialty.name, specialty.id] }
     @specialties_array.unshift(['Pick a Specialty',""])
+    @states_array = State.order(:abbrev).map { |state| [state.abbrev, state.id] }
+    @states_array.unshift(['Pick a State',""])
   end
 
   def search
     if !params[:q].nil?
-      if !params[:specialty].nil? && !params[:specialty].empty?
+      passed_specialty = !params[:specialty].nil? && !params[:specialty].empty?
+      passed_state = !params[:state].nil? && !params[:state].empty?
+
+      if passed_specialty && passed_state
+        @providers = Provider.joins(:specialties).joins(:addresses).includes(:hospitals,:organizations).where('specialty_id = ? AND state_id = ?',params[:specialty],params[:state]).find_with_index(params[:q])
+        @specialty_passed = Specialty.find(params[:specialty])
+        @state_passed = State.find(params[:state])
+      elsif passed_specialty
         @providers = Provider.joins(:specialties).includes(:hospitals,:organizations,:addresses).where('specialty_id = ?',params[:specialty]).find_with_index(params[:q])
         @specialty_passed = Specialty.find(params[:specialty])
+      elsif passed_state 
+        @providers = Provider.joins(:addresses).includes(:hospitals,:organizations,:specialties).where('state_id = ?',params[:state]).find_with_index(params[:q])
+        @state_passed = State.find(params[:state])
       else
         @providers = Provider.find_with_index(params[:q])
       end
