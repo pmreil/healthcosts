369431ac096b7bd78a883b053badaf3bffe2781b
Paul Reilly <preilly@teladoc.com>
2015-11-02 08:47:26 -0500

    updated more support for hcpcs

--- app/controllers/hcpcss_controller.rb
+++ app/controllers/hcpcss_controller.rb
@@ -10,15 +10,12 @@ class HcpcssController < ApplicationController
 
   # GET /hcpcs/1
   def show
-    @hcpcs = Hcpcs.find(params[:id],:include => "aliases")
-    @icd10s = @drg.icd10s
-    @thetitle = "Compare Medical billing costs for "+@drg.uc_description+" in U.S. Hospitals"
-    @count = @drg.costs.count
-    @costs =  @drg.costs.order('average_covered_charges desc').includes(:hospital).limit(10)
-    @average_covered_charges = @costs.average('average_covered_charges')
-    @average_total_payments = @costs.average('average_total_payments')
-    @states = State.find(:all,:order => "name")
-    @regions = Hospital.select('referral_region').group(:referral_region).count
+    @hcpcs = Hcpcs.find(params[:id])
+    @thetitle = "Compare Medical billing costs for "+@Hcpcs.uc_description+" by U.S. Physicians"
+    @count = @hcpcs.providers_costs.count
+    #@costs =  @hcpcs.providers_costs.order('average_covered_charges desc').includes(:physician).limit(10)
+    #@average_covered_charges = @costs.average('average_covered_charges')
+    #@average_total_payments = @costs.average('average_total_payments')
   end
 
 end
